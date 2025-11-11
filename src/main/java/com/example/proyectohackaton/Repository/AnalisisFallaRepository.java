7a0f0f85f39d1f5eeebaf456ca45f75d22da1b10
Marlon Garzón <marlongarzonmedina845@gmail.com>
2025-11-11 11:05:30 -0500

    Correcciones

--- src/main/java/com/example/proyectohackaton/Repository/AnalisisFallaRepository.java
+++ src/main/java/com/example/proyectohackaton/Repository/AnalisisFallaRepository.java
@@ -1,5 +1,6 @@
 package com.example.proyectohackaton.Repository;
 
+import com.example.proyectohackaton.Entity.AnalisisFalla;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AnalisisFallaRepository extends JpaRepository<AnalisisFalla,Long> {
